12d7edae15d13b64d15b3a4f48b7322b58a0eef3
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-01-04 14:56:17 +0100

    PlayerRepository interface created as RestRepository

--- src/main/java/com/codeoftheweb/salvo/Player.java
+++ src/main/java/com/codeoftheweb/salvo/Player.java
@@ -1,9 +1,23 @@
 package com.codeoftheweb.salvo;
 
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// In JPA, an Entity class is equivalent to a row of a database.
+@Entity
 public class Player {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+
+    private long id;
     private String firstName;
     private String lastName;
+    private String userName;
 
     // You must define a default (no-argument) constructor for any entity class. That's what JPA will call to create new instances
     public Player () {};
---
+++ src/main/java/com/codeoftheweb/salvo/PlayerRepository.java
@@ -0,0 +1,21 @@
+package com.codeoftheweb.salvo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
+
+/*
+ A Repository class is analogous to a table, i.e., it is a class that manages a collection of instances.
+ The interface is a JpaRepository, which is in turn an extension of CrudRepository.
+ PlayerRepository is defined to be a JpaRepository that manages instances of Player.
+ Spring will create an actual class with code that implements this interface.
+ The PlayerRepository class is responsible for persisting instances of Person to some database, and retrieving
+ instances.
+ With this separation, if another class just needs to work with Person instances, it can do so, without any database. If a web application needs to maintain a database, it does so by interacting with a PersonRepository.
+*/
+
+@RepositoryRestResource
+// ~ turns PersonRepository into a Rest Repository.
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+    List<Player> findByLastName(String lastName);
+}
