dca80b6aedced60bbe9c0a82090d4a3021597c4e
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-01-10 14:27:04 +0100

    Front end connected and fetching sample data

--- src/main/salvo-webapp/src/App.js
+++ src/main/salvo-webapp/src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const [games, setGames] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchAllGames = async () => {
+      const url = "http://localhost:8080/api/games";
+      try {
+        let response = await fetch(url);
+        let data = await response.json();
+
+        setGames(data.games)
+        setIsLoading(isLoading => isLoading = !isLoading)
+      } catch (err) { console.log(err) }
+    }
+    fetchAllGames()
+  }, [])
+
+  console.log(games)
+  console.log(isLoading)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Whatever
-        </a>
-      </header>
+    <div>
+      <h1>Games Data</h1>
+      <ol>
+        {!isLoading && games.map((game, idx) => (
+          <li key={idx}>
+            <ul>
+              <li>created: {game.created}</li>
+              {game.gamePlayers.map((gamePlayer, i) => (
+                <li key={i}>player email: {gamePlayer.player.player_email}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
