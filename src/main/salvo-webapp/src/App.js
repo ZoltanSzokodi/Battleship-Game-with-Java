2edfd912874b9aa319127fdd0103ff5ba4632dfc
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-01-10 14:51:57 +0100

    React Router added -> /web/games

--- src/main/salvo-webapp/src/App.js
+++ src/main/salvo-webapp/src/App.js
@@ -1,43 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React from "react"
+import Games from './components/Games'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
-  const [games, setGames] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchAllGames = async () => {
-      const url = "http://localhost:8080/api/games";
-      try {
-        let response = await fetch(url);
-        let data = await response.json();
-
-        setGames(data.games)
-        setIsLoading(isLoading => isLoading = !isLoading)
-      } catch (err) { console.log(err) }
-    }
-    fetchAllGames()
-  }, [])
-
-  console.log(games)
-  console.log(isLoading)
   return (
-    <div>
-      <h1>Games Data</h1>
-      <ol>
-        {!isLoading && games.map((game, idx) => (
-          <li key={idx}>
-            <ul>
-              <li>created: {game.created}</li>
-              {game.gamePlayers.map((gamePlayer, i) => (
-                <li key={i}>player email: {gamePlayer.player.player_email}</li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ol>
-    </div>
-  );
+    <Router>
+      <React.Fragment>
+        <Route exact path="/web/games" component={Games} />
+      </React.Fragment>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/main/salvo-webapp/src/components/Games.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+
+function Games() {
+  const [games, setGames] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchAllGames = async () => {
+      const url = "http://localhost:8080/api/games";
+      try {
+        let response = await fetch(url);
+        let data = await response.json();
+
+        setGames(data.games)
+        setIsLoading(isLoading => isLoading = !isLoading)
+      } catch (err) { console.log(err) }
+    }
+    fetchAllGames()
+  }, [])
+
+  console.log(games)
+  return (
+    <div>
+      <h1>Games Data</h1>
+      <ol>
+        {!isLoading && games.map((game, idx) => (
+          <li key={idx}>
+            <ul>
+              <li>created: {game.created}</li>
+              {game.gamePlayers.map((gamePlayer, i) => (
+                <li key={i}>
+                  player email: {gamePlayer.player.player_email}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default Games
